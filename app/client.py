6e27a728e3173cdfad71e485cf1c2dac5320a366
JordenChang <jordenchang55@gmail.com>
2023-03-26 20:57:52 -0700

    Add backend socket

--- app/client.py
+++ app/client.py
@@ -1,11 +1,16 @@
+import asyncio
 import json
 import logging
 import os
+import threading
 import time
+from json import JSONDecodeError
 from threading import Event
+from typing import Callable
 
 import openai
 import speech_recognition as sr
+import websockets
 
 from .buffer import Buffer
 from .constants import DEFAULT_CHUNK, DEFAULT_SAMPLE_RATE, EXIT_KEYWORDS
@@ -134,3 +139,59 @@ class MicClient:
 
     def stop(self):
         self._stop_listening()
+
+
+class WebSocketClient:
+    def __init__(self):
+        self._stop_future = None
+        self.socket = None
+        self.socket_callback = None
+        self._running_thread = None
+
+    def start(self, callback: Callable[[str | object, bool], None]):
+        """
+        Start the web socket and listen to the update from the clients.
+        :param callback: the handler function for the  messages from the client. It will pass object or string based on
+         the type of the original message. The second parameter indicates if the message is a JSON.
+        """
+        self._running_thread = threading.Thread(name="ws", target=self._start)
+        self.socket_callback = callback
+        self._running_thread.start()
+
+    def stop(self):
+        if self._running_thread:
+            self._stop_future.set_result(True)
+            self._running_thread.join()
+            self._running_thread = None
+            self._stop_future = None
+            self.socket_callback = None
+
+        else:
+            logging.warning("The server did not start yet...")
+
+    def send(self, message, event_type="text_update"):
+        logging.debug("Send: %s" % message)
+        if self.socket:
+            json_string = json.dumps({
+                "message": message,
+                "event": event_type,
+            })
+            asyncio.run(self.socket.send(json_string))
+
+    def _start(self):
+        asyncio.run(self._start_server())
+
+    async def _handle_websocket(self, websocket):
+        self.socket = websocket
+        async for message in websocket:
+            try:
+                payload = json.loads(message)
+                self.socket_callback(payload, True)
+            except JSONDecodeError:
+                self.socket_callback(message, False)
+
+    async def _start_server(self):
+        self._stop_future = asyncio.Future()
+        async with websockets.serve(self._handle_websocket, "localhost", 8000):
+            logging.debug("WebSocket server listening on ws://localhost:8000")
+            await self._stop_future
