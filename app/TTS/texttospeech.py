c6469d161ddec76667750fe03517b3032c5be68e
JordenChang <jordenchang55@gmail.com>
2023-03-26 23:14:48 -0700

    Add audio stream playing

--- app/TTS/texttospeech.py
+++ app/TTS/texttospeech.py
@@ -1,7 +1,10 @@
+import io
 import os
 import sys
 
 import google.cloud.texttospeech as tts
+import sounddevice as sd
+import soundfile as sf
 
 dir_path = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(dir_path)
@@ -17,7 +20,20 @@ class SpeechClient:
         )
         self._audio_config = tts.AudioConfig(audio_encoding=tts.AudioEncoding.LINEAR16)
 
+    def speak(self, text):
+        # TODO: No sound here
+        data = self._get_speech_data(text)
+        sound_data, samplerate = sf.read(io.BytesIO(data))
+        print(sound_data)
+        sd.play(sound_data, samplerate=samplerate)
+
     def speak_to_file(self, text, filename):
+        data = self._get_speech_data(text)
+        with open(filename, "wb") as out:
+            out.write(data)
+            print(f'Generated speech saved to "{filename}"')
+
+    def _get_speech_data(self, text):
         tts_input = tts.SynthesisInput()
         tts_input.text = text
         response = self.tts_client.synthesize_speech(
@@ -25,12 +41,9 @@ class SpeechClient:
             voice=self._voice_params,
             audio_config=self._audio_config,
         )
-        print(f"Response: {response}")
-        with open(filename, "wb") as out:
-            out.write(response.audio_content)
-            print(f'Generated speech saved to "{filename}"')
+        return response.audio_content
 
 
 if __name__ == '__main__':
     client = SpeechClient()
-    client.speak_to_file("What is the temperature in New York?", "en-US-Studio-O.wav")
+    client.speak("What is the temperature in New York?")
