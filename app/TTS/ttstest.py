2520291dccb0d8676e4a97e22087f1326c6180b9
JordenChang <jordenchang55@gmail.com>
2023-04-12 23:28:52 -0700

    Update TTS test

--- app/TTS/ttstest.py
+++ app/TTS/ttstest.py
@@ -1,38 +1,66 @@
-import numpy as np
-import soundfile as sf
-import yaml
+import time
 
+import sounddevice as sd
 import tensorflow as tf
-
-from tensorflow_tts.inference import TFAutoModel
 from tensorflow_tts.inference import AutoProcessor
+from tensorflow_tts.inference import TFAutoModel
 
+tacotron2 = TFAutoModel.from_pretrained("tensorspeech/tts-tacotron2-baker-ch", name="tacotron2")
 # initialize fastspeech2 model.
-fastspeech2 = TFAutoModel.from_pretrained("tensorspeech/tts-fastspeech2-ljspeech-en")
-
+fastspeech2 = TFAutoModel.from_pretrained("tensorspeech/tts-fastspeech2-baker-ch", name="fastspeech2")
 
 # initialize mb_melgan model
-mb_melgan = TFAutoModel.from_pretrained("tensorspeech/tts-mb_melgan-ljspeech-en")
-
+mb_melgan = TFAutoModel.from_pretrained("tensorspeech/tts-mb_melgan-baker-ch", name="mb_melgan")
 
 # inference
-processor = AutoProcessor.from_pretrained("tensorspeech/tts-fastspeech2-ljspeech-en")
+processor = AutoProcessor.from_pretrained("tensorspeech/tts-tacotron2-baker-ch")
 
-input_ids = processor.text_to_sequence("Recent research at Harvard has shown meditating for as little as 8 weeks, can actually increase the grey matter in the parts of the brain responsible for emotional regulation, and learning.")
-# fastspeech inference
 
-mel_before, mel_after, duration_outputs, _, _ = fastspeech2.inference(
-    input_ids=tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
-    speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
-    speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
-    f0_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
-    energy_ratios =tf.convert_to_tensor([1.0], dtype=tf.float32),
-)
+def do_synthesis(input_text, text2mel_model, vocoder_model, text2mel_name, vocoder_name):
+    input_ids = processor.text_to_sequence(input_text, inference=True)
 
-# melgan inference
-audio_before = mb_melgan.inference(mel_before)[0, :, 0]
-audio_after = mb_melgan.inference(mel_after)[0, :, 0]
+    # text2mel part
+    if text2mel_name == "TACOTRON":
+        _, mel_outputs, stop_token_prediction, alignment_history = text2mel_model.inference(
+            tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+            tf.convert_to_tensor([len(input_ids)], tf.int32),
+            tf.convert_to_tensor([0], dtype=tf.int32)
+        )
+    elif text2mel_name == "FASTSPEECH2":
+        mel_before, mel_outputs, duration_outputs, _, _ = text2mel_model.inference(
+            tf.expand_dims(tf.convert_to_tensor(input_ids, dtype=tf.int32), 0),
+            speaker_ids=tf.convert_to_tensor([0], dtype=tf.int32),
+            speed_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
+            f0_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
+            energy_ratios=tf.convert_to_tensor([1.0], dtype=tf.float32),
+        )
+    else:
+        raise ValueError("Only TACOTRON, FASTSPEECH2 are supported on text2mel_name")
 
-# save to file
-sf.write('./audio_before.wav', audio_before, 22050, "PCM_16")
-sf.write('./audio_after.wav', audio_after, 22050, "PCM_16")
+    # vocoder part
+    if vocoder_name == "MB-MELGAN":
+        # tacotron-2 generate noise in the end symtematic, let remove it :v.
+        if text2mel_name == "TACOTRON":
+            remove_end = 1024
+        else:
+            remove_end = 1
+        audio = vocoder_model.inference(mel_outputs)[0, :-remove_end, 0]
+    else:
+        raise ValueError("Only MB_MELGAN are supported on vocoder_name")
+
+    if text2mel_name == "TACOTRON":
+        return audio.numpy()
+    else:
+        return audio.numpy()
+
+
+input_text = "我认为人工智能会带来很多便利和发展机会，但确实也可能对某些行业造成影响，需要我们注重转型和转变。不过，技术的进步总是不可避免的，我们应该适时调整自己的态度和思路，去适应这个新的变化。"
+sentences = input_text.split('。')
+
+for s in sentences:
+    print("Read: %s" % s)
+    start = time.time()
+    audios = do_synthesis(s, tacotron2, mb_melgan, "TACOTRON", "MB-MELGAN")
+    print("Inference: %.2f" % (time.time() - start))
+    sd.play(audios, 22050)
+    sd.wait()
