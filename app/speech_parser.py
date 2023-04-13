4f79d7a98136fda9391caad83b4e03c45c0041ed
qwe789qwec <qwe789qwec@gmail.com>
2023-04-13 13:08:27 +0900

    speech_parser update openai

--- app/speech_parser.py
+++ app/speech_parser.py
@@ -1,7 +1,15 @@
+import os
+import sys
 import time
-
+import json
+import openai
 import speech_recognition as sr
 
+dir_path = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(dir_path)
+with open('app/resources/api_key.json') as f:
+    data = json.load(f)
+    openai.api_key = data['GPT-api-key']
 
 def callback(recognizer, audio):
     print("Audio data length: %d" % (len(audio.get_raw_data()),))
@@ -28,7 +36,8 @@ def listen_mic():
             # for testing purposes, we're just using the default API key
             # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
             # instead of `r.recognize_google(audio)`
-            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+            OPENAI_API_KEY = openai.api_key
+            print("Google Speech Recognition thinks you said " + r.recognize_whisper_api(audio, api_key=OPENAI_API_KEY))
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
         except sr.RequestError as e:
@@ -36,5 +45,3 @@ def listen_mic():
 
 
 listen_mic()
-while True:
-    time.sleep(0.1)
