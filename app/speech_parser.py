0c5d634899b0b435f9daa5ca4e48c136ac384a76
JordenChang <jordenchang55@gmail.com>
2023-04-12 20:54:14 -0700

    Switch foreground listener

--- app/speech_parser.py
+++ app/speech_parser.py
@@ -17,12 +17,22 @@ def callback(recognizer, audio):
 
 
 def listen_mic():
+    # obtain audio from the microphone
     r = sr.Recognizer()
-    mic = sr.Microphone()
+    with sr.Microphone() as source:
+        print("Say something!")
+        audio = r.listen(source)
 
-    with mic as source:
-        r.adjust_for_ambient_noise(source)
-    r.listen_in_background(mic, callback=callback)
+        # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
 
 listen_mic()
