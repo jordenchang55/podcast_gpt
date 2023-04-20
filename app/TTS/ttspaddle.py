9440f0e429d9961b83a34c700c918659c2fc2670
Wei-Sheng Wang <qwe789qwec@gmail.com>
2023-04-20 14:13:26 +0900

    update tts

--- app/TTS/ttspaddle.py
+++ app/TTS/ttspaddle.py
@@ -1,3 +1,8 @@
 from paddlespeech.cli.tts.infer import TTSExecutor
 tts = TTSExecutor()
-tts(text="中文語音合成測試。", output="output.wav")
+tts(
+    text="Linux Bonding（也稱為 Linux 套接字綁定，或簡稱 Bonding）是一種技術，用於將兩個或多個網絡接口卡（NIC）組合成單個邏輯接口。這個邏輯接口的好處是它提供了更高的帶寬、更好的容錯能力和更高的可用性。", 
+    # am="fastspeech2_aishell3",
+    # voc="hifigan_aishell3",
+    # lang="zh", 
+    output="output_api.wav")
