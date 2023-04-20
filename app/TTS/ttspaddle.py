6260dbbecac000fa8ec6373a4114cb81dc38a879
qwe789qwec <qwe789qwec@gmail.com>
2023-04-20 18:41:54 +0900

    update tts parameter

--- app/TTS/ttspaddle.py
+++ app/TTS/ttspaddle.py
@@ -1,8 +1,21 @@
-from paddlespeech.cli.tts.infer import TTSExecutor
-tts = TTSExecutor()
-tts(
-    text="Linux Bonding（也稱為 Linux 套接字綁定，或簡稱 Bonding）是一種技術，用於將兩個或多個網絡接口卡（NIC）組合成單個邏輯接口。這個邏輯接口的好處是它提供了更高的帶寬、更好的容錯能力和更高的可用性。", 
-    # am="fastspeech2_aishell3",
-    # voc="hifigan_aishell3",
-    # lang="zh", 
-    output="output_api.wav")
+import paddle
+from paddlespeech.cli.tts import TTSExecutor
+tts_executor = TTSExecutor()
+wav_file = tts_executor(
+    text="Linux Bonding（也稱為 Linux 套接字綁定，或簡稱 Bonding）是一種技術，用於將兩個或多個網絡接口卡（NIC）組合成單個邏輯接口。",
+    output="output_api.wav",
+    am="fastspeech2_mix",
+    am_config=None,
+    am_ckpt=None,
+    am_stat=None,
+    spk_id=0, # spk_id = 167,
+    phones_dict=None,
+    tones_dict=None,
+    speaker_dict=None,
+    voc="hifigan_male",
+    voc_config=None,
+    voc_ckpt=None,
+    voc_stat=None,
+    lang="mix",
+    use_onnx=True,
+    device=paddle.get_device())
