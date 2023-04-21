97191a6c977c4c8c094b5c1740fc5d613374c9bb
Wei-Sheng Wang <qwe789qwec@gmail.com>
2023-04-21 14:02:05 +0900

    add auto sound read

--- app/TTS/ttspaddle.py
+++ app/TTS/ttspaddle.py
@@ -1,9 +1,14 @@
+import logging
 import paddle
 from paddlespeech.cli.tts import TTSExecutor
+from playsound import playsound
+
+filename = "output_api.wav"
+
 tts_executor = TTSExecutor()
 wav_file = tts_executor(
-    text="Linux Bonding（也稱為 Linux 套接字綁定，或簡稱 Bonding）是一種技術，用於將兩個或多個網絡接口卡（NIC）組合成單個邏輯接口。",
-    output="output_api.wav",
+    text="我认为A,I会带来很多便利和发展机会，但确实也可能对某些行业造成影响，需要我们注重转型和转变。不过，技术的进步总是不可避免的，我们应该适时调整自己的态度和思路，去适应这个新的变化。",
+    output=filename,
     am="fastspeech2_male",
     am_config=None,
     am_ckpt=None,
@@ -19,3 +24,6 @@ wav_file = tts_executor(
     lang="mix",
     use_onnx=True,
     device=paddle.get_device())
+
+logging.debug(f'Generated speech saved to "{filename}"')
+playsound(filename)
