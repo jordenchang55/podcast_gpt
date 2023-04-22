cf0ac47a487d2b820d2b817c194837374b5459b2
JordenChang <jordenchang55@gmail.com>
2023-04-21 20:35:10 -0700

    Separate model loading and inference

--- app/TTS/ttspaddle.py
+++ app/TTS/ttspaddle.py
@@ -1,29 +1,48 @@
 import logging
+import time
+
 import paddle
 from paddlespeech.cli.tts import TTSExecutor
+from paddlespeech.cli.tts.infer import ONNX_SUPPORT_SET
+from paddlespeech.resource import CommonTaskResource
 from playsound import playsound
 
-filename = "output_api.wav"
+output = "output_api.wav"
 
 tts_executor = TTSExecutor()
-wav_file = tts_executor(
-    text="我认为A,I会带来很多便利和发展机会，但确实也可能对某些行业造成影响，需要我们注重转型和转变。不过，技术的进步总是不可避免的，我们应该适时调整自己的态度和思路，去适应这个新的变化。",
-    output=filename,
-    am="fastspeech2_male",
-    am_config=None,
+
+am = "fastspeech2_male"
+spk_id = 0  # spk_id = 167 for male,
+voc = "pwgan_male"
+lang = "mix"
+device = paddle.get_device()
+tts_executor.task_resource = CommonTaskResource(
+    task='tts', model_format='onnx')
+assert (
+        am in ONNX_SUPPORT_SET and voc in ONNX_SUPPORT_SET
+), f'the am and voc you choose, they should be in {ONNX_SUPPORT_SET}'
+tts_executor._init_from_path_onnx(
+    am=am,
     am_ckpt=None,
-    am_stat=None,
-    spk_id=0, # spk_id = 167 for male,
     phones_dict=None,
     tones_dict=None,
     speaker_dict=None,
-    voc="pwgan_male",
-    voc_config=None,
+    voc=voc,
     voc_ckpt=None,
-    voc_stat=None,
-    lang="mix",
-    use_onnx=True,
-    device=paddle.get_device())
-
-logging.debug(f'Generated speech saved to "{filename}"')
-playsound(filename)
+    lang=lang,
+    device=device,
+)
+sentences = [
+    "我认为A,I会带来很多便利和发展机会",
+    "但确实也可能对某些行业造成影响",
+    "需要我们注重转型和转变",
+    "不过，技术的进步总是不可避免的",
+    "我们应该适时调整自己的态度和思路，去适应这个新的变化。"
+]
+for text in sentences:
+    start = time.time()
+    tts_executor.infer_onnx(text=text, lang=lang, am=am, spk_id=spk_id)
+    print("Infer time: %.2f - [%s]" % (time.time() - start, text))
+    res = tts_executor.postprocess_onnx(output=output)
+    logging.debug(f'Generated speech saved to "{output}"')
+    playsound(output)
